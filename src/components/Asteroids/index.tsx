76c0dada0b70910a5a2050831cffea51b06df1bd
zakrjv <zakr.jv@gmail.com>
2023-09-22 15:10:13 +0300

    add adaptive

--- src/components/Asteroids/index.tsx
+++ src/components/Asteroids/index.tsx
@@ -2,7 +2,7 @@ import Asteroid from '@/components/Asteroid';
 
 export default function Asteroids() {
   return (
-    <section className="ml-14">
+    <section className="ml-14 md:w-[402px] md:m-auto md:pl-10">
       <h1 className="mb-2 text-title">Ближайшие подлёты астероидов</h1>
       <div className="mb-6 font-bold">
         <button>в километрах</button> |{' '}
---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,13 @@
+import Send from '../UI/Send';
+
+export default function Cart() {
+  return (
+    <div className="fixed bottom-0 left-0 w-full flex justify-between p-4 bg-cart md:block md:w-auto md:left-auto md:top-32 md:right-28 md:bottom-auto md:p-4 md:rounded-3xl xl:right-64">
+      <div>
+        <p className="font-bold text-lg">Корзина</p>
+        <p className="md:mb-8">2 астероида</p>
+      </div>
+      <Send>Отправить</Send>
+    </div>
+  );
+}
