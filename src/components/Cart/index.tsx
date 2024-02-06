09a045ff0db5f31ca473019c3447cbba2e5a1cc8
zakrjv <zakr.jv@gmail.com>
2024-02-06 21:13:07 +0300

    add loading state to submit button

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -2,6 +2,8 @@
 import { useRouter } from 'next/navigation';
 import { useStore } from '@/store';
 import Main from '@/components/UI/Buttons/Main';
+import { useState } from 'react';
+import Spin from '@/components/UI/Spin';
 
 export default function Cart() {
   const cart = useStore((state) => state.cart);
@@ -9,15 +11,21 @@ export default function Cart() {
   const sendOrder = useStore((state) => state.sendOrder);
   const idAsteroids = Object.keys(cart);
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const handleSend = () => {
-    saveOrder();
-    sendOrder();
-    router.push('/cart');
+    setLoading(true);
+
+    setTimeout(() => {
+      router.push('/cart');
+      saveOrder();
+      sendOrder();
+      setLoading(false);
+    }, 1000);
   };
 
   return (
-    <div className="fixed bottom-0 left-0 w-full flex justify-between p-4 bg-cart md:block md:w-auto md:left-auto md:top-32 md:right-28 md:bottom-auto md:p-4 md:rounded-3xl xl:right-64">
+    <div className="fixed bottom-0 left-0 w-full flex justify-between p-4 bg-cart md:block md:min-w-[150px] md:w-auto md:left-auto md:top-32 md:right-28 md:bottom-auto md:p-4 md:rounded-3xl xl:right-64">
       <div>
         <p className="font-bold text-lg">Корзина</p>
         <p className="md:mb-8">
@@ -26,8 +34,11 @@ export default function Cart() {
             : 'пуста'}
         </p>
       </div>
-      <Main onClick={handleSend} isDisabled={idAsteroids.length === 0}>
-        Отправить
+      <Main
+        onClick={handleSend}
+        isDisabled={idAsteroids.length === 0 || loading}
+      >
+        {loading ? <Spin /> : 'Отправить'}
       </Main>
     </div>
   );
